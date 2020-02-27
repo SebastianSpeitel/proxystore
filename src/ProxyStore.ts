85483eaead455554b13d6638aec4a7646161561c
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-02-27 13:12:35 +0100

    refactor ProxyStore to allow for changing the root * added DeepProxy

--- src/ProxyStore.ts
+++ src/ProxyStore.ts
@@ -1,64 +1,145 @@
 import IOHandler from "./io/IOHandler";
 
 export interface ProxyStoreOptions<T extends object> {
-    init?: T | boolean;
+  init?: T | boolean;
 }
 
-export default class ProxyStore<T extends object = object> implements ProxyHandler<any> {
-    public declare proxy: T;
-    public declare handler: IOHandler<T>;
-    readonly #store: T;
+type HookKey = keyof ProxyHandler<any>;
+const hookKeys = Object.getOwnPropertyNames(Reflect) as HookKey[];
 
-    constructor(handler: string | IOHandler<T>, { init = true }: ProxyStoreOptions<T> = {}) {
-        if (typeof handler === 'string') {
-            throw TypeError('Handler string not implemented yet')
-        }
-        else {
-            this.handler = handler;
-        }
+type HookMap = {
+  [hook in HookKey]?: (
+    p: PropertyKey[],
+    ...args: any[]
+  ) => ReturnType<Required<ProxyHandler<any>>[hook]>;
+};
 
-        if (!init) {
-            this.#store = {} as T;
-        }
-        else if (init === true) {
-            this.#store = {} as T;
-            this.load()
-        }
-        else if (typeof init === 'object') {
-            this.#store = init;
-        }
-        else {
-            throw TypeError("Invalid type of init option")
-        }
+abstract class DeepProxy<T extends object = any> {
+  private readonly _paths: WeakMap<object, PropertyKey[]> = new WeakMap();
+  protected _root: T;
+  private readonly _handler: ProxyHandler<any>;
+  public declare proxy: T;
 
-        this.proxy = new Proxy<T>(this.#store, this);
+  constructor(root: T = {} as T) {
+    if (typeof root !== "object") {
+      throw TypeError("Root value has to be of type object");
     }
+    this._root = root;
 
-    save() {
-        this.handler.save(this.#store)
+    const handler: ProxyHandler<any> = {};
+    for (const hook of hookKeys) {
+      handler[hook] = (t: any, ...args: any[]) => {
+        const path = this._paths.get(t);
+        if (!path) throw Error("Path not found");
+        //console.log(hook, ["root", ...path].join("."));
+        return this[hook](
+          path,
+          //@ts-ignore
+          ...args
+        );
+      };
     }
+    this._handler = handler;
 
-    load() {
-        Object.assign(this.#store, this.handler.load())
-    }
+    this.proxy = this.addPath([], Object.getPrototypeOf(this._root));
+  }
 
-    set(target: any, prop: PropertyKey, val: any) {
-        target[prop] = val;
-        this.save()
-        return true;
-    }
+  protected addPath(path: PropertyKey[], proto: object | null) {
+    const t = Array.isArray(proto) ? [] : Object.create(proto);
+    this._paths.set(t, path);
+    return new Proxy(t, this._handler);
+  }
 
-    get(target: any, prop: PropertyKey): any {
-        const val: unknown = target[prop];
-        if (typeof val === "object" && val) {
-            return new Proxy(val, this);
-        }
-        return val;
-    }
+  getByPath(path: PropertyKey[]) {
+    return path.reduce((o, k) => o[k], this._root as any);
+  }
 
-    deleteProperty(target: any, prop: PropertyKey) {
-        delete target[prop];
-        this.save()
-        return true
+  getOwnPropertyDescriptor(
+    path: PropertyKey[],
+    prop: string | number | symbol
+  ): PropertyDescriptor | undefined {
+    const target = this.getByPath(path);
+    const descriptor = Reflect.getOwnPropertyDescriptor(target, prop);
+    if (!descriptor) return undefined;
+    return Object.assign(descriptor, { configurable: true });
+  }
+
+  get(path: PropertyKey[], prop: PropertyKey): any {
+    const target = this.getByPath(path);
+    const val: unknown = Reflect.get(target, prop);
+    if (typeof val === "object" && val) {
+      return this.addPath([...path, prop], Object.getPrototypeOf(val));
     }
+    return val;
+  }
+}
+interface DeepProxy extends Required<HookMap> {}
+for (const key of Object.getOwnPropertyNames(Reflect) as HookKey[]) {
+  if (DeepProxy.prototype[key]) continue;
+  DeepProxy.prototype[key] = function(
+    this: DeepProxy,
+    path: PropertyKey[],
+    ...args: any[]
+  ) {
+    const target = this.getByPath(path);
+    return Reflect[key](
+      target,
+      //@ts-ignore
+      ...args
+    );
+  };
+}
+
+export default class ProxyStore<T extends object = object> extends DeepProxy<
+  T
+> {
+  public declare readonly proxy: T;
+  public declare handler: IOHandler<T>;
+
+  constructor(
+    paramHandler: string | IOHandler<T>,
+    { init = true }: ProxyStoreOptions<T> = {}
+  ) {
+    let handler: IOHandler<T>;
+    if (typeof paramHandler === "string") {
+      throw TypeError("Handler string not implemented yet");
+    } else {
+      handler = paramHandler;
+    }
+    let root: T;
+    if (!init) {
+      root = {} as T;
+    } else if (init === true) {
+      root = handler.load();
+    } else if (typeof init === "object") {
+      root = init;
+    } else {
+      throw TypeError("Invalid type of init option");
+    }
+    super(root);
+
+    this.handler = handler;
+  }
+
+  save() {
+    this.handler.save(this._root as T);
+  }
+
+  load() {
+    this._root = this.handler.load();
+  }
+
+  //@ts-ignore
+  set(path: PropertyKey[], prop: PropertyKey, val: any) {
+    const ret = super.set(path, prop, val);
+    this.save();
+    return ret;
+  }
+
+  //@ts-ignore
+  deleteProperty(path: PropertyKey[], prop: PropertyKey) {
+    const ret = super.deleteProperty(path, prop);
+    this.save();
+    return ret;
+  }
 }
