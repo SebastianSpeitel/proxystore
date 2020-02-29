49f883146853ee181bd538f83265db3eb1586fbe
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-02-29 12:54:51 +0100

    added replacer and reviver to ProxyStoreJSON

--- src/proxystore/json.ts
+++ src/proxystore/json.ts
@@ -2,18 +2,22 @@ import ProxyStoreBase from "./base";
 import * as fs from "fs";
 
 type PrettyOption = Parameters<JSON["stringify"]>[2] | boolean;
+type Replacer = (this: any, key: string, value: any) => any | null;
+type Reviver = (this: any, key: string, value: any) => any | null;
 
 interface Options<T extends object> {
   path: string;
   init?: T | boolean;
   pretty?: PrettyOption;
   watch?: boolean;
+  replacer?: Replacer;
+  reviver?: Reviver;
 }
 
-function readJSON<T = any>(path: string, fallback?: T): T {
+function readJSON<T = any>(path: string, fallback?: T, reviver?: Reviver): T {
   try {
     const json = fs.readFileSync(path).toString();
-    return JSON.parse(json);
+    return JSON.parse(json, reviver);
   } catch (e) {
     if (typeof fallback === "undefined") throw e;
     return fallback;
@@ -23,21 +27,31 @@ function readJSON<T = any>(path: string, fallback?: T): T {
 function writeJSON(
   path: string,
   obj: any,
-  { pretty }: { pretty?: PrettyOption } = {}
+  { pretty }: { pretty?: PrettyOption } = {},
+  replacer?: Replacer
 ) {
   if (pretty === true) pretty = 2;
   else if (pretty === false) pretty = undefined;
-  fs.writeFileSync(path, JSON.stringify(obj, null, pretty));
+  fs.writeFileSync(path, JSON.stringify(obj, replacer, pretty));
 }
 
 export default class ProxyStore<T extends object> extends ProxyStoreBase<T> {
   private declare path: string;
   public declare pretty: PrettyOption;
+  public declare replacer?: Replacer;
+  public declare reviver?: Reviver;
   public declare watcher?: fs.FSWatcher;
 
   constructor(
     root: T = {} as T,
-    { init = false, path, pretty = false, watch = false }: Options<T> = {
+    {
+      init = false,
+      path,
+      pretty = false,
+      watch = false,
+      replacer,
+      reviver
+    }: Options<T> = {
       path: ""
     }
   ) {
@@ -46,9 +60,11 @@ export default class ProxyStore<T extends object> extends ProxyStoreBase<T> {
     }
     let r = root;
     if (init) {
-      r = readJSON(path, r);
+      r = readJSON(path, r, reviver);
     }
     super(r);
+    this.replacer = replacer;
+    this.reviver = reviver;
     this.path = path;
     this.pretty = pretty;
     if (watch) {
@@ -57,11 +73,11 @@ export default class ProxyStore<T extends object> extends ProxyStoreBase<T> {
   }
 
   load() {
-    this._root = readJSON(this.path, this._root);
+    this._root = readJSON(this.path, this._root, this.reviver);
   }
 
   async save() {
-    writeJSON(this.path, this._root, { pretty: this.pretty });
+    writeJSON(this.path, this._root, { pretty: this.pretty }, this.replacer);
   }
 
   //@ts-ignore
