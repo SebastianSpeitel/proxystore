8bb60e61e1b77cf39fd3b0dee5e34befd21dfdb6
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-03-11 19:54:48 +0100

    emit an esm version

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import ts from "rollup-plugin-typescript2";
 
-export default {
-  input: "src/index.ts",
-  output: {
-    format: "cjs",
-    dir: "dist",
-    exports: "named",
-    hoistTransitiveImports: false
-  },
-  preserveModules: true,
-  context: "this",
-  plugins: [ts({ tsconfigOverride: { compilerOptions: { rootDir: "src" } } })],
-  external: ["fs"]
-};
+function makeConfig() {
+  return {
+    input: "src/index.ts",
+    output: {
+      format: "cjs",
+      dir: "legacy",
+      exports: "named",
+      hoistTransitiveImports: false
+    },
+    preserveModules: true,
+    context: "this",
+    plugins: [
+      ts({ tsconfigOverride: { compilerOptions: { rootDir: "src" } } })
+    ],
+    external: ["fs"]
+  };
+}
+
+const cjsConfig = makeConfig();
+const esmConfig = makeConfig();
+esmConfig.output.format = "esm";
+esmConfig.output.dir = "dist";
+
+export default [cjsConfig, esmConfig];
